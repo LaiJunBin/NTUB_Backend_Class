107d98b00a83d967dd7e51cda0e8ba526bd11ea0
laijunbin <xyz607xx@gmail.com>
2019-01-23 02:34:53 +0800

    complete commit

--- django-post/posts/views.py
+++ django-post/posts/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
-from .serializers import PostSerializer, CommitSerializer
-from .models import Post, Commit
-
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .permissions import IsCreatorOrReadOnly, CanUpdateOrDeleteCommit
-from rest_framework import status
-from rest_framework import mixins
+from rest_framework.serializers import Serializer
+
+from .models import Commit, Post
+from .permissions import CanUpdateOrDeleteCommit, IsCreatorOrReadOnly
+from .serializers import CommitSerializer, PostSerializer
+
 
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated, IsCreatorOrReadOnly]
-    
+
     # def get_serializer_class(self):
     #     serializer = super().get_serializer_class()
     #     if self.request.method not in SAFE_METHODS:
     #         return CreatePostSerializer
-        
+
     #     return serializer
 
     def perform_create(self, serializer):
@@ -30,7 +30,10 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer = super().get_serializer_class()
         if self.action == 'commit':
             return CommitSerializer
-        
+
+        if self.action == 'like':
+            return Serializer
+
         return serializer
 
     @action(['PATCH'], True, permission_classes=[IsAuthenticated])
@@ -40,7 +43,7 @@ class PostViewSet(viewsets.ModelViewSet):
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
-        
+
         serializer = self.get_serializer(post)
         return Response(serializer.data)
 
@@ -52,8 +55,8 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save(creator=self.request.user, post=post)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
-class CommitViewSet(viewsets.ModelViewSet):
+
+class CommitViewSet(viewsets.ReadOnlyModelViewSet, mixins.UpdateModelMixin, mixins.RetrieveModelMixin):
     queryset = Commit.objects.all()
     serializer_class = CommitSerializer
     permission_classes = [IsAuthenticated, CanUpdateOrDeleteCommit]
@@ -61,14 +64,21 @@ class CommitViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
 
+    def get_serializer_class(self):
+        serializer = super().get_serializer_class()
+
+        if self.action == 'like':
+            return LikesSerializer
+
+        return serializer
+
     @action(['PATCH'], True, permission_classes=[IsAuthenticated])
     def like(self, request, pk):
         commit = self.get_object()
-        print(commit.likes.__dir__())
         if request.user in commit.likes.all():
             commit.likes.remove(request.user)
         else:
             commit.likes.add(request.user)
-        
+
         serializer = self.get_serializer(commit)
         return Response(serializer.data)
